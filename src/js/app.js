5875f6a0f8c92e12b1eefe9d14083b5b9ee0d42b
arnaud ferrand <arferrand@gmail.com>
2015-04-01 22:06:21 +0200

    home button  + synch sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -52,6 +52,7 @@ function sizeLayerControl() {
 
 function clearHighlight() {
   highlight.clearLayers();
+  bufferedLayer.clearLayers();
 }
 
 function sidebarClick(id) {
@@ -85,11 +86,12 @@ function syncSidebar() {
 }
 
 function getFeatureListContent(layer) {
-  if(layer.feature.properties.nom_station === '' ) {
-    layer.feature.properties.nom_station = layer.feature.properties.nom_porteur;
+  var name = layer.feature.properties.nom_porteur;
+  if(layer.feature.properties.nom_station !== '' ) {
+    name = name + ' ' +layer.feature.properties.nom_station;
   }
   return '<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng +
-    '"><td style="vertical-align: middle;"><i class="fa fa-chevron-left pull-left"></i></td><td style="vertical-align: middle;"><img width="24" height="24" src="img/electric_power.png"></td><td class="feature-name">' + layer.feature.properties.nom_station +
+    '"><td style="vertical-align: middle;"><i class="fa fa-chevron-left pull-left"></i></td><td style="vertical-align: middle;"><img width="24" height="24" src="img/electric_power.png"></td><td class="feature-name">' + name +
     '</td></tr>';
 }
 
@@ -154,8 +156,6 @@ $("#searchbox").keypress(function (e) {
 $("#featureModal").on("hidden.bs.modal", function (e) {
   $(document).on("mouseout", ".feature-row", clearHighlight);
 });
-
-
 
 /*******************/
 /* let's do maps ! */
@@ -243,89 +243,9 @@ var zoom = 6;
 map = L.map('leaflet-map', {
   center: [lat, lng],
   zoom: zoom,
-  zoomControl: false,
+  defaultExtentControl: true,
   layers: [topo, markers, highlight, bufferedLayer]
 });
-
-// custom zoom bar control that includes a Zoom Home function
-L.Control.zoomHome = L.Control.extend({
-    options: {
-        position: 'topleft',
-        zoomInText: '+',
-        zoomInTitle: 'Zoom in',
-        zoomOutText: '-',
-        zoomOutTitle: 'Zoom out',
-        zoomHomeText: '<i class="fa fa-home" style="line-height:1.65;"></i>',
-        zoomHomeTitle: 'Zoom home'
-    },
-
-    onAdd: function (map) {
-        var controlName = 'gin-control-zoom',
-            container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
-            options = this.options;
-
-        this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
-        controlName + '-in', container, this._zoomIn);
-        this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
-        controlName + '-home', container, this._zoomHome);
-        this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
-        controlName + '-out', container, this._zoomOut);
-
-        this._updateDisabled();
-        map.on('zoomend zoomlevelschange', this._updateDisabled, this);
-
-        return container;
-    },
-
-    onRemove: function (map) {
-        map.off('zoomend zoomlevelschange', this._updateDisabled, this);
-    },
-
-    _zoomIn: function (e) {
-        this._map.zoomIn(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomOut: function (e) {
-        this._map.zoomOut(e.shiftKey ? 3 : 1);
-    },
-
-    _zoomHome: function (e) {
-        map.setView([lat, lng], zoom);
-    },
-
-    _createButton: function (html, title, className, container, fn) {
-        var link = L.DomUtil.create('a', className, container);
-        link.innerHTML = html;
-        link.href = '#';
-        link.title = title;
-
-        L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
-            .on(link, 'click', L.DomEvent.stop)
-            .on(link, 'click', fn, this)
-            .on(link, 'click', this._refocusOnMap, this);
-
-        return link;
-    },
-
-    _updateDisabled: function () {
-        var map = this._map,
-            className = 'leaflet-disabled';
-
-        L.DomUtil.removeClass(this._zoomInButton, className);
-        L.DomUtil.removeClass(this._zoomOutButton, className);
-
-        if (map._zoom === map.getMinZoom()) {
-            L.DomUtil.addClass(this._zoomOutButton, className);
-        }
-        if (map._zoom === map.getMaxZoom()) {
-            L.DomUtil.addClass(this._zoomInButton, className);
-        }
-    }
-});
-// add the new control to the map
-var zoomHome = new L.Control.zoomHome();
-zoomHome.addTo(map);
-
 
 L.control.locate().addTo(map);
 
@@ -340,6 +260,11 @@ searchControl.on('results', function(data) {
     .openOn(map);
 });
 /////////////////
+
+/* Filter sidebar feature list to only show features in current map bounds */
+map.on("moveend", function (e) {
+  syncSidebar();
+});
 
 
 map.on('click', onMapClick);
